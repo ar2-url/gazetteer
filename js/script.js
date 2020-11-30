516ff88dc5430f8bbb38b5acbe22b3b2b6e2630f
czeslaw187 <czeslaw187@users.noreply.github.com>
2020-11-30 09:17:03 +0000

    1

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ $(window).on('load', function () {
 
     mapboxgl.accessToken = token;
     let satMap = new mapboxgl.Map({
-    container: 'map',
+    container: 'mapid',
     zoom: 9,
     center: [137.9150899566626, 36.25956997955441],
     style: 'mapbox://styles/mapbox/satellite-v9'
